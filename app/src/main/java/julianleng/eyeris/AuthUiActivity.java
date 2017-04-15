8db9c5789bfd4da74166101a5b09b3dd36723f48
Julian Leng <leng.julian1@gmail.com>
2017-04-15 14:10:25 -0700

    Revamped login, added logout feature in the new options menu in the top right of the screen

---
+++ app/src/main/java/julianleng/eyeris/AuthUiActivity.java
@@ -0,0 +1,170 @@
+
+package julianleng.eyeris;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.annotation.StringRes;
+import android.support.annotation.StyleRes;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.AuthUI.IdpConfig;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AuthUiActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 100;
+
+    @BindView(R.id.default_theme)
+    RadioButton mUseDefaultTheme;
+
+    @BindView(R.id.green_theme)
+    RadioButton mUseGreenTheme;
+
+    @BindView(R.id.purple_theme)
+    RadioButton mUsePurpleTheme;
+
+    @BindView(R.id.dark_theme)
+    RadioButton mUseDarkTheme;
+
+    @BindView(R.id.email_provider)
+    RadioButton mUseEmailProvider;
+
+    @BindView(R.id.google_provider)
+    RadioButton mUseGoogleProvider;
+
+    @BindView(R.id.facebook_provider)
+    RadioButton mUseFacebookProvider;
+
+    @BindView(R.id.sign_in)
+    Button mSignIn;
+
+    @BindView(R.id.root)
+    View mRootView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            startActivity(SignedInActivity.createIntent(this, null));
+            finish();
+        }
+
+        setContentView(R.layout.auth_ui_layout);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.sign_in)
+    public void signIn(View view) {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setTheme(getSelectedTheme())
+                        .setLogo(R.drawable.firebase_auth_120dp)
+                        .setProviders(getSelectedProviders())
+                        .setIsSmartLockEnabled(true)
+                        .setAllowNewEmailAccounts(true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            handleSignInResponse(resultCode, data);
+        }
+    }
+
+    @MainThread
+    private void handleSignInResponse(int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        // Successfully signed in
+        if (resultCode == ResultCodes.OK) {
+            finish();
+        } else {
+            // Sign in failed
+            if (response == null) {
+                // User pressed back button
+                showSnackbar(R.string.sign_in_cancelled);
+                return;
+            }
+
+            if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                showSnackbar(R.string.no_internet_connection);
+            }
+        }
+    }
+
+    @MainThread
+    @StyleRes
+    private int getSelectedTheme() {
+        if (mUseDefaultTheme.isChecked()) {
+            return AuthUI.getDefaultTheme();
+        }
+
+        if (mUsePurpleTheme.isChecked()) {
+            return R.style.PurpleTheme;
+        }
+
+        if (mUseDarkTheme.isChecked()) {
+            return R.style.DarkTheme;
+        }
+
+        return R.style.GreenTheme;
+    }
+
+
+    @MainThread
+    private List<IdpConfig> getSelectedProviders() {
+        List<IdpConfig> selectedProviders = new ArrayList<>();
+
+        if (mUseEmailProvider.isChecked()) {
+            selectedProviders.add(new IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
+        }
+
+        if (mUseFacebookProvider.isChecked()) {
+            selectedProviders.add(
+                    new IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER)
+                            .build());
+        }
+
+        if (mUseGoogleProvider.isChecked()) {
+            selectedProviders.add(
+                    new IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER)
+                            .build());
+        }
+
+        return selectedProviders;
+    }
+
+
+    @MainThread
+    private void showSnackbar(@StringRes int errorMessageRes) {
+        Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
+    }
+
+    public static Intent createIntent(Context context) {
+        Intent in = new Intent();
+        in.setClass(context, AuthUiActivity.class);
+        return in;
+    }
+}
--- app/src/main/java/julianleng/eyeris/MainActivity.java
+++ app/src/main/java/julianleng/eyeris/MainActivity.java
@@ -1,6 +1,7 @@
 package julianleng.eyeris;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -10,71 +11,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableStringBuilder;
+import android.text.style.TypefaceSpan;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
-    private HomePageAdapter adapter;
     private BottomNavigationView bottomNavigation;
     private Fragment fragment;
     private FragmentManager fragmentManager;
-    private List<ScrollablePosts> listItems;public static final String POSTS = "posts";
-    private RecyclerView mPostsRecyclerView;
-    private LinearLayoutManager mLinearLayoutManager;
-    private DatabaseReference mFirebaseDatabaseReference;
-    private FirebaseRecyclerAdapter<ScrollablePosts, HomePageAdapter.HomeViewHolder> mFirebaseAdapter;
 
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseUser mFirebaseUser;
     private static final String TAG = "MainActivity";
-    private String mUsername;
-    private String mPhotoUrl;
-    private GoogleApiClient mGoogleApiClient;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //user authentication
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        if (mFirebaseUser == null){
-            startActivity(new Intent(this, SignInActivity.class));
-            finish();
-            return;
-        }else{
-            mUsername = mFirebaseUser.getDisplayName();
-            if(mFirebaseUser.getPhotoUrl() != null){
-                mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
-            }
-        }
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API)
-                .build();
+        Intent signin = new Intent(this, AuthUiActivity.class);
+        this.startActivity(signin);
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.add(R.id.main_container, new HomeFragment());
+        tx.commit();
 
         bottomNavigation = (BottomNavigationView) findViewById(R.id.navigation);
         fragmentManager = getSupportFragmentManager();
-        fragment = new HomeFragment();
-        final FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.main_container, fragment).commit();
         bottomNavigation.setOnNavigationItemSelectedListener((new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -87,7 +70,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                         fragment = new NotificationFragment();
                         break;
                     case R.id.menu_post:
-                        fragment = new PostFragment();
+                            fragment = new PostFragment();
                         break;
                     case R.id.menu_profile:
                         fragment = new ProfileFragment();
@@ -95,12 +78,50 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                     case R.id.menu_search:
                         fragment = new SearchFragment();
                         break;
+                    default:
+                        fragment=new HomeFragment();
+                        break;
                 }
                 final FragmentTransaction transaction = fragmentManager.beginTransaction();
                 transaction.replace(R.id.main_container, fragment).commit();
                 return true;
             }
         }));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_card, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        final Intent intent = new Intent(this, AuthUiActivity.class);
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.menu_logout:
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    startActivity(intent);
+                                }
+                            }
+                        });
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
