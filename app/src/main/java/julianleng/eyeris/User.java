9861c73439b3d86896a8d8fef31f786bc42b0461
Thomas <trleavy@gmail.com>
2017-04-16 21:40:03 -0700

    changed ui

--- app/src/main/java/julianleng/eyeris/User.java
+++ app/src/main/java/julianleng/eyeris/User.java
@@ -1,5 +1,7 @@
 package julianleng.eyeris;
 
+import java.util.ArrayList;
+
 /**
  * Created by Thomas on 4/11/2017.
  */
@@ -7,6 +9,10 @@ package julianleng.eyeris;
 public class User {
 
     private String username;
+    private String userID;
+    private ArrayList<String> userPosts = new ArrayList<>();
+    private ArrayList<String> userComments = new ArrayList<>();
+    private ArrayList<String> savedPosts = new ArrayList<>();
     private double searchRadius;
 
 
